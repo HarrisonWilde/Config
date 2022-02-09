3eccd9e1c45b1c050df94ed10fe23aadc75c3a56
HarrisonWilde <harrisondwilde@outlook.com>
2022-02-09 19:07:03 +0000

    Fixing julia colours

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -6,19 +6,29 @@ OhMyREPL.enable_pass!("RainbowBrackets", false)
 
 scheme = SyntaxHighlighter.ColorScheme()
 
-SyntaxHighlighter.symbol!(scheme, Crayon(foreground = :magenta))
-SyntaxHighlighter.comment!(scheme, Crayon(foreground = :dark_gray, italics = true))
-SyntaxHighlighter.string!(scheme, Crayon(foreground = :green))
+SyntaxHighlighter.symbol!(scheme, Crayon(foreground = :blue, bold = true))
+SyntaxHighlighter.comment!(scheme, Crayon(foreground = :white))
+SyntaxHighlighter.string!(scheme, Crayon(foreground = :light_blue))
 SyntaxHighlighter.call!(scheme, Crayon(foreground = :blue))
-SyntaxHighlighter.op!(scheme, Crayon(foreground = :yellow))
-SyntaxHighlighter.keyword!(scheme, Crayon(foreground = :magenta, bold = true))
+SyntaxHighlighter.op!(scheme, Crayon(foreground = :red))
+SyntaxHighlighter.keyword!(scheme, Crayon(foreground = :red, bold = true))
 SyntaxHighlighter.macro!(scheme, Crayon(foreground = :blue, italics = true))
-SyntaxHighlighter.function_def!(scheme, Crayon(foreground = :blue))
+SyntaxHighlighter.function_def!(scheme, Crayon(foreground = :magenta))
 SyntaxHighlighter.text!(scheme, Crayon(foreground = :default))
 SyntaxHighlighter.error!(scheme, Crayon(foreground = :red))
-SyntaxHighlighter.argdef!(scheme, Crayon(foreground = :blue, italics = true))
-SyntaxHighlighter.number!(scheme, Crayon(foreground = :red))
+SyntaxHighlighter.argdef!(scheme, Crayon(foreground = :blue))
+SyntaxHighlighter.number!(scheme, Crayon(foreground = :blue))
 
 SyntaxHighlighter.add!("Terminal", scheme)
 colorscheme!("Terminal")
 
+function funcdef(x::Float64, y::Int64)
+    y = 100_000
+    x = :foo
+    s = "I am a happy string"
+    c = `mycmd`
+    @time 1+1
+    #= Comments look like this =#
+    z = funccall(x, y)
+    5 * 3 + 2 - 1
+end
